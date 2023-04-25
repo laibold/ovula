e1e78b78d226fda4d21da2260a9664cce1ad5a5a
Marvin Laibold <marvin.laibold@aoe.com>
2023-04-25 18:55:43 +0200

    vite ⚡️

--- src/pages/Inputs/components/Form/index.tsx
+++ src/pages/Inputs/components/Form/index.tsx
@@ -36,11 +36,6 @@ const weekdayByIndex: Record<number, string> = {
 const FormikInputsForm = ({ touched, errors }: FormikProps<FormValues>) => {
   return (
     <Wrapper>
-      <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum.{" "}
-      </p>
       <Form>
         <label>
           Beginn deiner Periode
